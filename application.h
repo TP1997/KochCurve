990e72ca691b7a956da27772bbdca691d6378daa
TP1997 <47488769+TP1997@users.noreply.github.com>
2019-03-05 01:16:26 +0200

    Add files via upload

--- application.h
+++ application.h
@@ -1,24 +1,27 @@
 #ifndef APPLICATION_H_INCLUDED
 #define APPLICATION_H_INCLUDED
 #include <SFML/Graphics.hpp>
-#include "Line.h"
+#include "circle.h"
+#include "curve.h"
 #include <vector>
 class Application{
-    sf::RenderWindow &window;
-    std::vector<Line*> lines;
-    int maxGenerations;
+    sf::RenderWindow window;
+    int scrW;
+    int scrH;
+    int rows; int cols;
+    float angle;
+    int frameRate;
+    std::vector<Circle*> circleRow;
+    std::vector<Circle*> circleCol;
+    Curve ***curves2;
+
 public:
-    Application(sf::RenderWindow &rw);
+    Application(int rows, int cols);
     ~Application();
     void mainloop();
-    int readCommand();
-    int readInteger()throw(const char*);
-    int castInteger(std::string value);
-    void selectVisualization();
-    void animate(std::vector<Line*> &lines, int currGen);
-    void handleWinEvents();
-};
+    void draw();
+    void update();
 
-std::vector<Line*> createNewGeneration(std::vector<Line*> &oldGen, int gen);
+};
 
 #endif // APPLICATION_H_INCLUDED
