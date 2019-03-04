045bf8b97766be06281743929b05e1c1680d0dc1
TP1997 <47488769+TP1997@users.noreply.github.com>
2019-03-05 01:26:13 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -10,20 +10,6 @@ int main()
     window.create(sf::VideoMode(800, 900), "SFML works!");
     Application app(window);
     app.mainloop();
-    /*
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen()){
-        sf::Event event;
-        while (window.pollEvent(event)){
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }*/
     return 0;
 }
